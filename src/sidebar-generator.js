2806317d9b8a8b1c2cdfb98e9483a5486da3e64b
john.xlm <60260750+JFWooten4@users.noreply.github.com>
2026-04-28 09:58:56 -0400

    gg

--- src/sidebar-generator.js
+++ src/sidebar-generator.js
@@ -1,5 +1,5 @@
-import fs from "fs";
-import path from "path";
+const fs = require("fs");
+const path = require("path");
 
 module.exports = async ({ defaultSidebarItemsGenerator, ...args }) => {
   const docs = args.docs.filter(
