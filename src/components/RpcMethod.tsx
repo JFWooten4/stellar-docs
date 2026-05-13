105bb79ee5f8dfb5ed17ea0bc238995ed0e4602a
john.xlm <60260750+JFWooten4@users.noreply.github.com>
2026-05-13 19:57:51 -0400

    fix fee-bump tx errors this overrides

--- src/components/RpcMethod.tsx
+++ src/components/RpcMethod.tsx
@@ -1,11 +1,18 @@
-import Method from "@stellar/open-rpc-docs-react";
 import React from "react";
 import type { MethodObject } from "@open-rpc/meta-schema";
-
-const CodeBlock = require("@theme/CodeBlock").default;
-const Tabs = require("@theme/Tabs").default;
-const TabItem = require("@theme/TabItem").default;
+import BrowserOnly from "@docusaurus/BrowserOnly";
 
 export function RpcMethod({ method }: { method: MethodObject }) {
-  return <Method method={method} components={{ CodeBlock, Tabs, TabItem }} />;
+  return (
+    <BrowserOnly fallback={<div>Loading method details...</div>}>
+      {() => {
+        const Method = require("@stellar/open-rpc-docs-react").default;
+        const CodeBlock = require("@theme/CodeBlock").default;
+        const Tabs = require("@theme/Tabs").default;
+        const TabItem = require("@theme/TabItem").default;
+
+        return <Method method={method} components={{ CodeBlock, Tabs, TabItem }} />;
+      }}
+    </BrowserOnly>
+  );
 }
